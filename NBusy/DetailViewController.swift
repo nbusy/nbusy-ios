61dee9e0a1ef557048606ebeeffb7cda741ece78
Teoman Soygul <tsoygul@gmail.com>
2015-09-30 17:26:01 +0200

    remove detailDescriptionLabel property

--- NBusy/DetailViewController.swift
+++ NBusy/DetailViewController.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    
+    // MARK: Properties
 
 
     var detailItem: AnyObject? {
@@ -15,9 +15,6 @@ class DetailViewController: UIViewController {
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.valueForKey("timeStamp")!.description
-            }
         }
     }
 
